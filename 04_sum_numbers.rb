c11910bd99f9e3654c83b8d843552f99d46bb42c
RaphaelPl <160218@tuta.iol>
2020-02-16 12:36:26 +1000

    validation added, solved

--- 04_sum_numbers.rb
+++ 04_sum_numbers.rb
@@ -10,8 +10,18 @@
 # Check your solution by running the tests:
 # ruby tests/04_sum_numbers_test.rb
 
+# def array_length_check(numbers)
+#   if numbers.length < 0
+#     return false
+#   else 
+#     return true
+# end
+
 def sum_numbers (numbers)
   # Your code here
+    numbers.map(&:to_i).reduce(0, :+)
+
+
 end
 
 
@@ -23,3 +33,4 @@ end
 
 # Hint:
 # You could test what happens when you call sum_numbers([])?
+sum_numbers([1,1,1])
